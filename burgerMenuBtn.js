a936c91bb7bcead2d322a05dad0cc90e4fc2e9f0
Ksenia <kseniapopova02@gmail.com>
2023-03-10 20:15:26 +0100

    write: style menu__open

--- burgerMenuBtn.js
+++ burgerMenuBtn.js
@@ -1,7 +1,15 @@
+import { toggleClass } from "./function.js";
+
 const cross = document.getElementById("burgerButton");
+const body = document.getElementById("body");
+// const crossWrapper = getElementById("buttonWrapper");
 
 cross.addEventListener("click", (e) => {
   e.preventDefault();
   cross.classList.toggle("opened");
   cross.setAttribute("aria-expanded", cross.classList.contains("opened"));
+});
+
+cross.addEventListener("click", () => {
+  toggleClass(body, "menu__open");
 });
