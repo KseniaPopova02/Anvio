1465a6a00b66600691e58444da344ab1db2672d6
Ksenia <kseniapopova02@gmail.com>
2023-03-10 19:49:46 +0100

    write: html, css and js for language menu

---
+++ language.js
@@ -0,0 +1,28 @@
+import { toggleClass } from "./function.js";
+
+const languageBtn = document.getElementById("languageBtn");
+const headerLanguage = document.getElementById("headerLanguage");
+const nestedLi = document.getElementById("languageNestedLi");
+
+//showing block of languages
+languageBtn.addEventListener("click", () => {
+  toggleClass(headerLanguage, "header__language-show");
+});
+
+//hiding block of languages if you click outside of it
+window.addEventListener("click", (e) => {
+  {
+    const path = e.composedPath().includes(nestedLi);
+
+    if (!path) {
+      headerLanguage.classList.remove("header__language-show");
+      nestedLi.classList.remove("active__li");
+    }
+  }
+});
+
+nestedLi.addEventListener("click", () => {
+  toggleClass(nestedLi, "active__li");
+});
+
+//I also want to do clickable language options
