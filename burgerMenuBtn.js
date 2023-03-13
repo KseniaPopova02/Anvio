fd9982faf317e1b4a8908ec317e62bb47d352acf
Ksenia <kseniapopova02@gmail.com>
2023-03-13 12:43:15 +0100

    write: html, css, js for the mobile events

--- burgerMenuBtn.js
+++ burgerMenuBtn.js
@@ -3,9 +3,24 @@ import { toggleClass } from "./function.js";
 //cross
 const cross = document.getElementById("burgerButton");
 const mobileMenu = document.getElementById("mobileMenu");
+
 //region nav
 const mobileRegionBtn = document.getElementById("mobileRegionNavLocation");
 const mobileRegionContent = document.getElementById("mobileRegionContent");
+
+//language nav
+const languageBtn = document.getElementById("languageBtn");
+const mobileLanguages = document.getElementById("mobileLanguages");
+
+//mobile entertainments
+const mobileEntertainments = document.getElementById("mobileEntertainments");
+const mobileMenuEntertainments = document.getElementById(
+  "mobileMenuEntertainments"
+);
+
+//mobile events
+const mobileEvents = document.getElementById("mobileEvents");
+const mobileEventsContent = document.getElementById("mobileEventsContent");
 
 //open and close animation
 cross.addEventListener("click", (e) => {
@@ -21,4 +36,31 @@ cross.addEventListener("click", () => {
 //mobile region nav selection animation
 mobileRegionBtn.addEventListener("click", () => {
   toggleClass(mobileRegionContent, "mobile__region-location-show");
+});
+
+mobileRegionBtn.addEventListener("click", () => {
+  toggleClass(mobileRegionBtn, "active__li");
+});
+
+//mobile language selection
+languageBtn.addEventListener("click", () => {
+  toggleClass(mobileLanguages, "mobile__language-show");
+});
+
+//mobile entertainments
+mobileEntertainments.addEventListener("click", () => {
+  toggleClass(mobileMenuEntertainments, "mobile__menu-entertainments-show");
+});
+
+mobileEntertainments.addEventListener("click", () => {
+  toggleClass(mobileEntertainments, "active__li");
+});
+
+//mobile events
+mobileEvents.addEventListener("click", () => {
+  toggleClass(mobileEventsContent, "mobile__events-show");
+});
+
+mobileEvents.addEventListener("click", () => {
+  toggleClass(mobileEvents, "active__li");
 });
