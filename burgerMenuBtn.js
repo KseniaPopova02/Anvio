59e9e7a8ffc0938cb4c112009b205189a2d2033a
Ksenia <kseniapopova02@gmail.com>
2023-03-13 11:08:32 +0100

    add: toggleClass() to the mobile region selection

--- burgerMenuBtn.js
+++ burgerMenuBtn.js
@@ -1,8 +1,13 @@
 import { toggleClass } from "./function.js";
 
+//cross
 const cross = document.getElementById("burgerButton");
 const mobileMenu = document.getElementById("mobileMenu");
+//region nav
+const mobileRegionBtn = document.getElementById("mobileRegionNavLocation");
+const mobileRegionContent = document.getElementById("mobileRegionContent");
 
+//open and close animation
 cross.addEventListener("click", (e) => {
   e.preventDefault();
   cross.classList.toggle("opened");
@@ -11,4 +16,9 @@ cross.addEventListener("click", (e) => {
 
 cross.addEventListener("click", () => {
   toggleClass(mobileMenu, "mobile__burger-menu-show");
+});
+
+//mobile region nav selection animation
+mobileRegionBtn.addEventListener("click", () => {
+  toggleClass(mobileRegionContent, "mobile__region-location-show");
 });
