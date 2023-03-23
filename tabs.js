bca7cd236e65f3c6c8268d1e530f4c0913da5af1
Ksenia <kseniapopova02@gmail.com>
2023-03-23 19:57:43 +0100

    add: border and display:none

--- tabs.js
+++ tabs.js
@@ -2,20 +2,20 @@ const tabContent = document.getElementsByClassName("activities__tabs-content");
 const tab = document.getElementsByClassName("activities__tabs-btn");
 const tabsWrapper = document.getElementById("tabs");
 
-const hideTabsContent = (array) => {
-  for (let i = array; i < tabContent.length; i++) {
+const hideTabsContent = (num) => {
+  for (let i = num; i < tabContent.length; i++) {
     tabContent[i].classList.remove("show");
     tabContent[i].classList.add("hide");
     tab[i].classList.remove("active__tab");
   }
 };
 
-const showTabsContent = (item) => {
-  if (tabContent[item].classList.contains("hide")) {
+const showTabsContent = (num) => {
+  if (tabContent[num].classList.contains("hide")) {
     hideTabsContent(0);
-    tab[item].classList.add("active__tab");
-    tabContent[item].classList.remove("hide");
-    tabContent[item].classList.add("show");
+    tab[num].classList.add("active__tab");
+    tabContent[num].classList.remove("hide");
+    tabContent[num].classList.add("show");
   }
 };
 
