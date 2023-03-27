691c4b9644bb3cc20393e01598c979a21c0768cc
Ksenia <kseniapopova02@gmail.com>
2023-03-27 13:07:19 +0200

    add: slider

--- slider.js
+++ slider.js
@@ -14,3 +14,20 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper__arrow-prev",
   },
 });
+
+const swiper2 = new Swiper(".swiper2", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".pag__2",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper__arrow-next",
+    prevEl: ".swiper__arrow-prev",
+  },
+});
